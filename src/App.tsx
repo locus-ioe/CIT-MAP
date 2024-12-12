c3867ad52c96db3d2ac39f5159fbd040dffe6939
Divya <karkidivya5@gmail.com>
2024-12-12 21:45:22 +0545

    type error added

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import MapComponent from './components/MapComponent';
 import axios from 'axios';
+import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
 
-const App = () => {
-    const [schools, setSchools] = useState([]);
-    const [geoJsonData, setGeoJsonData] = useState(null);
+const App: React.FC = () => {
+    const [schools, setSchools] = useState<any[]>([]);
+    const [geoJsonData, setGeoJsonData] = useState<FeatureCollection<Geometry, GeoJsonProperties> | null>(null);
 
     useEffect(() => {
         // Load GeoJSON data
         axios.get('maps/nepal-with-provinces.geojson')
-            .then(response => setGeoJsonData(response.data))
-            .catch(error => console.error('Error loading the GeoJSON data:', error));
+            .then((response : any) => setGeoJsonData(response.data))
+            .catch((error:any) => console.error('Error loading the GeoJSON data:', error));
     }, []);
 
-    const handleProvinceClick = (provinceName) => {
+    const handleProvinceClick = (provinceName: string) => {
         console.log("Fetching schools for:", provinceName);
         axios.get('data/schools.json')
-            .then(response => {
-                const provinceData = response.data.Provinces.find(p => p.name === provinceName);
+            .then((response:any) => {
+                const provinceData = response.data.Provinces.find(
+                    (p: { name: string; schools: any[] }) => p.name === provinceName
+                );
                 console.log("Found schools:", provinceData ? provinceData.schools : "No schools found");
                 setSchools(provinceData ? provinceData.schools : []);
             })
-            .catch(error => console.error('Error fetching schools data:', error));
+            .catch((error:any) => console.error('Error fetching schools data:', error));
     };
 
     return (
         <div style={{ position: 'relative', height: '100vh', width: '100%' }}>
-            <MapComponent 
-                geoJsonData={geoJsonData} 
-                onProvinceClick={handleProvinceClick} 
-                schools={schools} 
-            />
+            {geoJsonData && (
+                <MapComponent 
+                    geoJsonData={geoJsonData} 
+                    onProvinceClick={handleProvinceClick} 
+                    schools={schools} 
+                />
+            )}
         </div>
     );
 };
