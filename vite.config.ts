3094bdc2ed72df2515843ecfd6d310b537df813d
Ujj1225 <ujjwaljha1225@gmail.com>
2024-12-29 03:56:35 +0545

    Added sitemap, robots.txt and meta keywords

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -7,5 +8,8 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({ algorithm: "brotliCompress", threshold: 1024 }),
+  ],
 }));
